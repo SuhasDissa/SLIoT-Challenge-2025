c65f512b06054394f75ff671609a95d99f537a05
MaathavanJkr <maathumaathavan@gmail.com>
2024-12-16 13:06:26 +0530

    categories and align

--- src/components/section/Prizes.jsx
+++ src/components/section/Prizes.jsx
@@ -4,44 +4,100 @@ import "./prizes.scss";
 const Prizes = () => {
   return (
     <div className="mt-10 w-full mx-auto text-white body-container">
-      <h1>Awards</h1>
-      <div class="container">
-        <div class="product">
-          <div class="effect-1"></div>
-          <div class="effect-2"></div>
-          <div class="content">
-            <div class="text-black">3rd</div>
-          </div>
-          <span class="title">
-            LKR 10,000
-          </span>
+      <h1 className="text-6xl text-center">Prizes</h1>
+
+      <div className="flex flex-wrap justify-center items-center gap-x-4 md:gap-x-10 mt-10 px-4 md:px-0">
+        <div className="button-container">
+          <button className="blob-btn">
+            School Category
+            <span className="blob-btn__inner">
+              <span className="blob-btn__blobs">
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+              </span>
+            </span>
+          </button>
         </div>
 
-        <div class="bg"></div>
-        <div class="product">
-          <div class="effect-1"></div>
-          <div class="effect-2"></div>
-          <div class="content">
-            <div class="text-black">1st</div>
+        <div className="button-container">
+          <button className="blob-btn">
+            University Category
+            <span className="blob-btn__inner">
+              <span className="blob-btn__blobs">
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+              </span>
+            </span>
+          </button>
+        </div>
+
+        <div className="button-container">
+          <button className="blob-btn">
+            Open Category
+            <span className="blob-btn__inner">
+              <span className="blob-btn__blobs">
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+                <span className="blob-btn__blob"></span>
+              </span>
+            </span>
+          </button>
+        </div>
+
+      </div>
+
+      <div className="w-full flex flex-wrap-reverse justify-center items-center text-center mt-6 px-4 md:px-0">
+        <div className="product order-3 lg:order-2">
+          <div className="effect-1"></div>
+          <div className="effect-2"></div>
+          <div className="content">
+            <div className="text-black">1st</div>
           </div>
-          <span class="title">
+          <span className="title">
             LKR 25,000
           </span>
         </div>
+        <div className="bg"></div>
 
-        <div class="bg"></div>
-        <div class="product">
-          <div class="effect-1"></div>
-          <div class="effect-2"></div>
-          <div class="content">
-          <div class="text-black">2nd</div>
+        <div className="product order-2 lg:order-3">
+          <div className="effect-1"></div>
+          <div className="effect-2"></div>
+          <div className="content">
+            <div className="text-black">2nd</div>
           </div>
-          <span class="title">
+          <span className="title">
             LKR 15,000
           </span>
         </div>
-        <div class="bg"></div>
+        <div className="bg"></div>
+
+        <div className="product order-1 lg:order-1">
+          <div className="effect-1"></div>
+          <div className="effect-2"></div>
+          <div className="content">
+            <div className="text-black">3rd</div>
+          </div>
+          <span className="title">
+            LKR 10,000
+          </span>
+        </div>
+        <div className="bg"></div>
       </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+            <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+          </filter>
+        </defs>
+      </svg>
     </div>
   );
 };
