3bec29625561d80e1a45503eb68effdf32dec58a
Suhas Dissanayake <suhasdissa@gmail.com>
2025-12-15 18:14:29 +0530

    fix: 3D globe position

--- src/components/ui/GridGlobe.jsx
+++ src/components/ui/GridGlobe.jsx
@@ -393,7 +393,7 @@ export function GlobeDemo() {
   ];
 
   return (
-    <div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full">
+    <div className="flex items-center justify-center absolute -left-5 top-36 md:top-40 w-full h-full -z-10">
       <div className="max-w-7xl h-[32rem] mx-auto w-full relative overflow-hidden px-4">
         {/* <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" /> */}
         <div className="absolute w-full h-96 md:h-[32rem] z-0">
